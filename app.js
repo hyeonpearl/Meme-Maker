5057a357c90621dab4b7519e35247a3df2f92c72
hyeonpearl <hc9064@gmail.com>
2022-11-27 17:59:46 +0900

    refactor : prettier 적용

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const lineWidth = document.querySelector('#line-width');
-const colorOptions = Array.from(document.getElementsByClassName('color-option'));
+const colorOptions = Array.from(
+  document.getElementsByClassName('color-option')
+);
 const modeBtn = document.querySelector('#mode-btn');
 const eraseBtn = document.querySelector('#erase-btn');
 const resetBtn = document.querySelector('#reset-btn');
@@ -21,82 +23,82 @@ let isPainting = false;
 let isFilling = false;
 
 function onDraw(event) {
-    if (isPainting) {
-        ctx.lineTo(event.offsetX, event.offsetY);
-        ctx.stroke();
-        return;
-    }
-    ctx.moveTo(event.offsetX, event.offsetY);
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 function startPainting() {
-    isPainting = true;
+  isPainting = true;
 }
 function canclePainting() {
-    isPainting = false;
-    ctx.beginPath();
+  isPainting = false;
+  ctx.beginPath();
 }
 function onLineWidthChange(event) {
-    ctx.lineWidth = event.target.value;
+  ctx.lineWidth = event.target.value;
 }
 function onColorChange(event) {
-    ctx.strokeStyle = event.target.value;
-    ctx.fillStyle = event.target.value;
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
 }
 function onColorClick(event) {
-    const colorValue = event.target.dataset.color; 
-    ctx.strokeStyle = colorValue;
-    ctx.fillStyle = colorValue;
-    color.value = colorValue;
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
 }
 function onModeClick() {
-    if (isFilling) {
-        isFilling = false;
-        modeBtn.innerText = '배경 채우기';
-    } else {
-        isFilling = true;
-        modeBtn.innerText = '그리기';
-    }
-}
-function onCanvasClick() {
-    if (isFilling) {
-        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    }
-}
-function onEraseClick() {
-    ctx.strokeStyle = 'white';
+  if (isFilling) {
     isFilling = false;
     modeBtn.innerText = '배경 채우기';
+  } else {
+    isFilling = true;
+    modeBtn.innerText = '그리기';
+  }
+}
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+function onEraseClick() {
+  ctx.strokeStyle = 'white';
+  isFilling = false;
+  modeBtn.innerText = '배경 채우기';
 }
 function onResetClick() {
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 function onFileChange(event) {
-    const file = event.target.files[0];
-    const url = URL.createObjectURL(file);
-    const image = new Image();
-    image.src = url;
-    image.onload = function() {
-        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        fileInput.value = null;
-    }
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
 }
 function onDoubleClick(event) {
-    const text = textInput.value;
-    if (text !== '') {
-        ctx.save();
-        ctx.lineWidth = 1;
-        ctx.font = '48px serif';
-        ctx.fillText(text, event.offsetX, event.offsetY);
-        ctx.restore();
-    }
+  const text = textInput.value;
+  if (text !== '') {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = '48px serif';
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
 }
 function onSaveClick() {
-    const url = canvas.toDataURL();
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = '내 그림.png';
-    a.click();
+  const url = canvas.toDataURL();
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'drawing.png';
+  a.click();
 }
 
 canvas.addEventListener('mousemove', onDraw);
